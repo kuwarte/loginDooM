31059e5e0079c788b685d76728366e290afc4bdd
Jasper Cuarte <hekiigyokuu@gmail.com>
2025-12-18 11:22:09 +0800

    fix next vulnerability issue

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    turbo: {
-      rules: {
-        '*.css': {
-          loaders: ['css-loader'],
-          as: '*.css',
-        },
-      },
-    },
-  },
 };
 
 export default nextConfig;
